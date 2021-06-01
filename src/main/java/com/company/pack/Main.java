629ca8463d40b223004c610f47af228412cbd866
Vonisak <ivan.kasinov@mail.ru>
2021-06-01 20:18:20 +0300

    настроил Travis CI #3

--- src/main/java/com/company/pack/Main.java
+++ src/main/java/com/company/pack/Main.java
@@ -1,4 +1,4 @@
-package com.company;
+package pack;
 
 import java.awt.event.*;
 import javax.swing.*;
--- src/test/java/com/company/pack/GaussCalcTest.java
+++ src/test/java/com/company/pack/GaussCalcTest.java
@@ -1,4 +1,4 @@
-package com.company;
+package pack;
 
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
